6c5d314d509595e69372b6cd797990a2fa15d3a2
Vinicius <vinicius.ferraz@gmail.com>
2020-04-21 03:22:38 -0300

    criando projetos

---
+++ marketplace/src/test/java/com/github/viniciusfcf/ifood/mp/NativeHelloResourceIT.java
@@ -0,0 +1,9 @@
+package com.github.viniciusfcf.ifood.mp;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeHelloResourceIT extends HelloResourceTest {
+
+    // Execute the same tests but in native mode.
+}
---
+++ pedido/src/test/java/com/github/viniciusfcf/ifood/pedido/NativeHelloResourceIT.java
@@ -0,0 +1,9 @@
+package com.github.viniciusfcf.ifood.pedido;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeHelloResourceIT extends HelloResourceTest {
+
+    // Execute the same tests but in native mode.
+}
