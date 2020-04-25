e5b924ab42c8d4d365e66fff6b5ba870055e1794
Vinicius <vinicius.ferraz@gmail.com>
2020-04-25 19:47:11 -0300

    salvando restaurante e localizacao

--- marketplace/src/main/java/com/github/viniciusfcf/ifood/mp/Restaurante.java
+++ marketplace/src/main/java/com/github/viniciusfcf/ifood/mp/Restaurante.java
@@ -1,5 +1,8 @@
 package com.github.viniciusfcf.ifood.mp;
 
+import io.vertx.mutiny.pgclient.PgPool;
+import io.vertx.mutiny.sqlclient.Tuple;
+
 public class Restaurante {
 
     public Long id;
@@ -11,5 +14,14 @@ public class Restaurante {
     @Override
     public String toString() {
         return "Restaurante [id=" + id + ", nome=" + nome + ", localizacao=" + localizacao + "]";
+    }
+
+    public void persist(PgPool pgPool) {
+        pgPool.preparedQuery("insert into localizacao (id, latitude, longitude) values ($1, $2, $3)",
+                Tuple.of(localizacao.id, localizacao.latitude, localizacao.longitude)).await().indefinitely();
+
+        pgPool.preparedQuery("insert into restaurante (id, nome, localizacao_id) values ($1, $2, $3)",
+                Tuple.of(id, nome, localizacao.id)).await().indefinitely();
+
     }
 }
--- marketplace/src/main/java/com/github/viniciusfcf/ifood/mp/RestauranteCadastrado.java
+++ marketplace/src/main/java/com/github/viniciusfcf/ifood/mp/RestauranteCadastrado.java
@@ -1,13 +1,19 @@
 package com.github.viniciusfcf.ifood.mp;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
 
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
+import io.vertx.mutiny.pgclient.PgPool;
+
 @ApplicationScoped
 public class RestauranteCadastrado {
+
+    @Inject
+    PgPool pgPool;
 
     @Incoming("restaurantes")
     public void receberRestaurante(String json) {
@@ -18,5 +24,7 @@ public class RestauranteCadastrado {
         System.out.println(json);
         System.out.println("------------------------");
         System.out.println(restaurante);
+
+        restaurante.persist(pgPool);
     }
 }
