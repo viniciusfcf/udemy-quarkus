5f72fb810898071088b1f7cfcdbe5999887568e4
Vinicius <vinicius.ferraz@gmail.com>
2020-05-15 19:58:46 -0300

    Corrigindo sub

--- cadastro/src/test/java/com/github/viniciusfcf/ifood/cadastro/RestauranteResourceTest.java
+++ cadastro/src/test/java/com/github/viniciusfcf/ifood/cadastro/RestauranteResourceTest.java
@@ -63,7 +63,6 @@ public class RestauranteResourceTest {
                 .body(dto)
                 .when().put("/restaurantes/{id}")
                 .then()
-                //Se der 403 aqui, é devido a chave publica do keyclock que pode ter alterado
                 .statusCode(Status.NO_CONTENT.getStatusCode())
                 .extract().asString();
 
